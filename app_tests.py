be3be5a56946a271d5cb877aa542fc4e62534e61
savvy-sam <samchege09@gmail.com>
2017-10-19 10:04:07 +0300

    [chore] added tests to the yummy app

--- app_tests.py
+++ app_tests.py
@@ -3,8 +3,27 @@ import unittest
 from app import app
 
 class TestApp(unittest.TestCase):
+
+    def setUp(self):
+	    self.app=app.test_client()
     
 
     def test_index(self):
         indx=self.app.get('/')
         self.assertEqual(indx.status_code, 200) 
+
+
+
+    def test_login(self):
+    	indx=self.app.get('/login')
+    	self.assertEqual(indx.status_code, 200)
+
+
+
+    def  test_signup(self):
+    	indx=self.app.get('/signup')
+    	self.assertEqual(indx.status_code, 200)
+
+    def test_recipes(self):
+    	indx=self.app.get('/recipes')
+    	self.assertEqual(indx.status_code, 200)
