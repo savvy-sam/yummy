eff801dfccb8e284e9c49c8860a601fc95689659
savvy-sam <samchege09@gmail.com>
2017-10-21 22:00:23 +0300

    [feature] add a route for displaying all users and recipes

--- app/model.py
+++ app/model.py
@@ -1,18 +1,20 @@
 import  uuid
 
 users_dict={'name':'initial','email':'initia'}
-
+users_index=[]
+recipes_index=[]
 
 class User(object):
 
     count=0
 
     def __init__(self,name,email, password, confirm):
-	    User.count+=1
-	    self.name=name
-	    self.email=email
-	    self.password=password
-	    self.confirm=confirm
+        users_index.append(self)
+        User.count+=1
+        self.name=name
+        self.email=email
+        self.password=password
+        self.confirm=confirm
 
 
     def set_id(self):
@@ -30,6 +32,8 @@ class Recipe(object):
 
 
     def __init__(self,title,content):
+    	Recipe.count+=1
+    	recipes_index.append(self)
         self.title=title
         self.content=content
 
@@ -39,4 +43,3 @@ class Recipe(object):
 
 
 
-    
--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
 from flask import render_template
 from flask import request
 from flask import redirect
-from wtforms import Form, StringField, TextAreaField, PasswordField , validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 
 from app import app
-from model import User
-from model import users_dict
-from model import Recipe
-#user=User('a','b','c','d')
+from app.model import User
+from app.model import Recipe
+from app.model import recipes_index
+
+
+users_index = []
+recipes_index = []
 
 @app.route('/')
 def index():
@@ -16,48 +19,48 @@ def index():
 
 @app.route('/login')
 def login():
-	return render_template('user_login.html')
+    return render_template('user_login.html')
 
 @app.route('/signup')
 def create():
-	return render_template('registration_form.html')
+    return render_template('registration_form.html')
 
 
 
 class RegisterForm(Form):
-	name=StringField('NAME', [validators.length(max=20)])
-	email=StringField("EMAIL", [validators.length(max=20)])
-	password=PasswordField("PASSWORD", [validators.DataRequired(), 
-		                                validators.EqualTo('confirm', message ="Your passwords do not match")])
+    name = StringField('NAME', [validators.length(max = 20)])
+    email = StringField("EMAIL", [validators.length(max = 20)])
+    password = PasswordField("PASSWORD", [validators.DataRequired(), 
+		                                 validators.EqualTo('confirm', 
+		                                 message = "Your passwords do not match")])
+    confirm = PasswordField('CONFIRM PASSWORD')
 
-	confirm=PasswordField('CONFIRM PASSWORD')
 
-
-@app.route('/register', methods=['GET', 'POST'] )
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
-    form=RegisterForm(request.form)
+    form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        user=User(form.name.data, form.email.data, form.password.data, form.confirm.data)
+        user = User(form.name.data, form.email.data, form.password.data, form.confirm.data)
         #form.populate_obj(user)
         #user.save()
+
+        users_index.append(user)
         user.add_user(user.name, user.email)
         return redirect('/')
-        #return user.name
-        
+        #return user.name    
     return render_template('register.html', form=form)
 
 
 @app.route('/user/update', methods=['GET', 'POST'])
 def update_user():
-    form=RegisterForm(request.form)
+    form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        user=User(form.name.data, form.email.data, form.password.data, form.confirm.data)
+        user = User(form.name.data, form.email.data, form.password.data, form.confirm.data)
         #form.populate_obj(user)
         #user.save()
         user.add_user(user.name, user.email)
         return redirect('/')
-        #return user.name
-        
+        #return user.name   
     return render_template('register.html', form=form)
 
 
@@ -65,44 +68,57 @@ def update_user():
 
 
 @app.route('/recipes')
-def list():
+def listing():
     return render_template('recipes_index.html')
 
 
 
 @app.route('/user/show')
 def show_user():
-	return (user.name, user.email)
+    return (user.name, user.email)
 
 
 
 class RecipeForm(Form):
-    title=StringField('TITLE', [validators.length(max=20)])
-    content=TextAreaField("CONTENT", [validators.length(max=500)])
+    title = StringField('TITLE', [validators.length(max=20)])
+    content = TextAreaField("CONTENT", [validators.length(max=500)])
     
 
-@app.route('/create/recipe',methods=['GET','POST'])
+@app.route('/create/recipe', methods = ['GET', 'POST'])
 def create_recipe():
-    form=RecipeForm(request.form)
+    form = RecipeForm(request.form)
     if request.method == 'POST' and form.validate():
-	    recipe=Recipe(form.title.data, form.content.data)
-	    return recipe.content
-
+        recipe = Recipe(form.title.data, form.content.data)
+        recipes_index.append(recipe)
+        return recipe.content
     return render_template('recipe.html', form=form)
 
 
 
-@app.route('/recipe/update', methods=['GET','POST'])
+@app.route('/recipe/update', methods = ['GET', 'POST'])
 def update_recipe():
-    form=RecipeForm(request.form)
+    form = RecipeForm(request.form)
     if request.method == 'POST' and form.validate():
-	    recipe=Recipe(form.title.data, form.content.data)
-	    return recipe.content
-
-    return render_template('recipe.html', form=form)
-
+        recipe = Recipe(form.title.data, form.content.data)
+        return recipe.content
+    return render_template('recipe.html', form = form)
 
 
 
-    
 
+@app.route('/users')
+def  people():
+    if len(users_index) >= 1:
+        for persons in users_index:
+            return persons.name
+
+    return "sorry No Users in Your App Yet"
+
+
+@app.route('/recipes/index')
+
+def all_recipes():
+    if len(recipes_index) >= 1:
+        for dish in recipes_index:
+            return dish.name
+    return "Dear User , You Dont Have Recipes Here Yet"
