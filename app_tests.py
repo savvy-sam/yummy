39ed8232707f315639a90b1366c3f741ac40854e
savvy-sam <samchege09@gmail.com>
2017-10-28 13:54:42 +0300

    [edit] remove tests for deleted routes

--- app_tests.py
+++ app_tests.py
@@ -27,18 +27,6 @@ class TestApp(unittest.TestCase):
 #Test whether  the route returns HTTP code 200
         self.assertEqual(indx.status_code, 200)
 
-    def  test_signup(self):
-        """Test whether this route renders the a page succesfully"""
-        indx = self.app.get('/signup')
-#Test whether  the route returns HTTP code 200
-        self.assertEqual(indx.status_code, 200)
-
-    def test_recipes(self):
-        """Test whether this route renders the a page succesfully"""
-        indx = self.app.get('/recipes')
-#Test whether  the route returns HTTP code 200
-        self.assertEqual(indx.status_code, 200)
-
     def test_class_creation(self):
         """This method tests whether an instance of User class can be created succesfully"""
         name = self.user.name
